cbe6dd68332332c749bec01170d55effe4b89406
Raúl <Ral@Marrero>
2023-01-19 10:55:53 +0000

    Version 1

---
+++ src/kata4/view/MailHistogramBuilder.java
@@ -0,0 +1,23 @@
+package kata4.view;
+
+import java.util.List;
+import kata4.model.Histogram;
+import kata4.model.Mail;
+/**
+ *
+ * @author Raúl Marrero Marichal
+ */
+
+public class MailHistogramBuilder {
+
+    public static Histogram<String> build(List<Mail> mails)
+    {
+        Histogram<String> histo = new Histogram<>();
+
+        for(Mail m : mails)
+            histo.increment(m.getDomain());
+
+        return histo;
+    }
+
+}
